dc118b37e9bd2f6c938a0a9e0891c0e5af67e940
ERNKw <112559107+ERNKw@users.noreply.github.com>
2022-11-22 15:55:54 -0300

    Update xss_automation.py

--- xss_automation.py
+++ xss_automation.py
@@ -11,9 +11,8 @@ import sys
 import time
 
 #executa o gauplus para pegar as urls e jogar no arquivo gau.txt
-os.system("cat urls.txt | gauplus > gau.txt")
-
-#executa o kxss e filtra com sed para pegar apenas as urls e jogar no arquivo kxss.txt
-os.system("cat gau.txt | kxss | sed 's/.*http/http/' >> kxss.txt")
-#executa o script de xss para cada url
-os.system("cat kxss.txt | dalfox pipe -S --mining-dict | tee  dalfox.txt")
+os.system("cat urls.txt | gauplus | uniq > gau.txt")
+#filtra o arquivo gau.txt com o kxss e sed para pegar apenas as urls e jogar no arquivo kxss.txt
+os.system("cat gau.txt | kxss | sed 's/.*http/http/' | uniq > kxss.txt")
+#executa o dalfox para fazer a verificação de cada url/parâmetro
+os.system("cat kxss.txt | dalfox pipe -S")
